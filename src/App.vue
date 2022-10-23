aaaf1757a5230fc246044df2fcf271735baeda0f
lumey <lumey@lumey.dev>
2022-10-22 22:57:41 -0400

    i have done enough work forf the day thank you where is my raise?

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Background from './components/background/background.vue';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+</script>
+<template>
+  <div id="app">
+    <Background />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/background/background.vue
@@ -0,0 +1,29 @@
+<template>
+    <canvas id="gradient-canvas" data-transition-in />
+</template>
+
+<script setup>
+import { Gradient } from './gradient.js';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+// Create your instance
+const gradient = new Gradient()
+
+// Call `initGradient` with the selector to your canvas
+gradient.initGradient('#gradient-canvas')
+});
+
+</script>
+
+<style scoped>
+#gradient-canvas {
+  width:100%;
+  height:100%;
+  --gradient-color-1: #dff1ff; 
+  --gradient-color-2: #6dbff5; 
+  --gradient-color-3: #5292ad;  
+  --gradient-color-4: #9fa7ff;
+  z-index: -1;
+}
+</style>
